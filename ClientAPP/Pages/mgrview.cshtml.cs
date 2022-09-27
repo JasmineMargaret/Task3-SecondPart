7c2ed9b926a7f9fd12796b08e115f0d44d280c55
Jasmine M <Jasmine@softura.com>
2022-09-27 13:25:29 +0530

    task 3

---
+++ ClientAPP/Pages/mgrview.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClientAPP.Pages
+{
+    public class mgrviewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ClientAPP/wwwroot/js/apicall.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(
+    function () {
+        $('#submit').click(function () {
+
+
+            $.ajax({
+                url: 'https://localhost:44354/Users/authenticate',
+                dataType: 'json',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    username: $('#Username').val(),
+                    password: $('#Password').val()
+                }),
+                processData: false,
+                success: function (data) {
+                    console.log(data);
+                    alert("Login Credential authenticated successfully")
+                    alert(data.role);
+                    if (data.role == "Manager") {
+                        window.location.replace('/mgrview')
+                    }
+                    else {
+                        window.location.replace('/wfmview')
+                    }
+                },
+                error: function () {
+                    alert("Login Credential Failed to authenticate")
+                }
+            });
+
+
+
+        })
+
+    }
+)
